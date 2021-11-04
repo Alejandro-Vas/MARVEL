1738a4d7ca32c2005507106b1350f4337bd36368
Alejandro-Vas <zended@yandex.ru>
2021-11-04 12:28:27 +0300

    fix: loading alt

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -2,7 +2,7 @@ import img from './Spinner-1s-200px.gif';
 
 const Spinner = () => {
     return (
-        <img style={{ display: 'block', objectFit: 'contain', margin: "0 auto"}}  src={img} alt="Error"/>
+        <img style={{ display: 'block', objectFit: 'contain', margin: "0 auto"}}  src={img} alt="loading..."/>
     )
 }
 
