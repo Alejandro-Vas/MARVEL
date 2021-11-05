5f54e6a920122fdb1437ac423131cd5c6284ba4b
Alejandro-Vas <zended@yandex.ru>
2021-11-06 02:19:03 +0300

    fix: Spinner

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 
 import useMarvelService from '../../services/MarvelService';
-import Spinner from '../../components/spinner/Spinner';
+import Spinner from '../Spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './singleComicPage.scss'
