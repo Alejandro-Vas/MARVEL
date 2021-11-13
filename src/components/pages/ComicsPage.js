c1de930938e011ab77f4353d4c3485c95500514e
Alejandro-Vas <zended@yandex.ru>
2021-11-13 12:45:25 +0300

    helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,19 @@
+import { Helmet } from "react-helmet";
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner"
+
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="descriprion"
+                    content="Page with comics"
+                    />
+                <title>Comics page</title>
+            </Helmet>        
             <AppBanner/>
             <ComicsList/>
         </>
